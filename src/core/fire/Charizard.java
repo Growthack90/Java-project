9a036beeeff76cccc330ca3eecd0ac1840b5a291
Growthack90 <hackmaker90@outlook.it>
2021-11-29 17:50:17 +0100

    inserito costruttore

--- src/core/fire/Charizard.java
+++ src/core/fire/Charizard.java
@@ -1,4 +1,14 @@
 package core.fire;
 
 public class Charizard {
+    private String name;
+    private Integer level;
+    private String weakness;
+
+    // costruttore
+    public Charizard(String name, Integer level, String weakness) {
+        this.name = name;
+        this.level = level;
+        this.weakness = weakness;
+    }
 }
